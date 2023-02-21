9306d0ef9b143fec6284c07d734aa0eb6b5f9853
liangzhensheng <1250564179@qq.com>
2023-02-21 11:48:00 +0800

    feat: :sparkles: 健身计划查询变成下拉菜单

--- gym-management-front/src/pages/admin/c-pages/gymInfo/index.vue
+++ gym-management-front/src/pages/admin/c-pages/gymInfo/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>gym</div>
+  <admin-page-layout title="场馆位置管理">
+    <el-table :data="tableData" style="width: 100%" size="large" border>
+      <el-table-column prop="username" label="用户名" />
+      <el-table-column prop="name" label="课程名" />
+      <el-table-column prop="detailText" label="课程详情" />
+      <el-table-column label="课程图">
+        <template #default="scope">
+          <div class="flex items-center">
+            <el-image :src="scope.row.img" fit="cover" />
+          </div>
+        </template>
+      </el-table-column>
+      <!-- <el-table-column fixed="right" label="操作" width="120">
+        <template #default="scope">
+          <el-button link type="primary" @click="handleDelete(scope)"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column> -->
+    </el-table>
+  </admin-page-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const tableData = reactive([]);
+</script>
 
 <style scoped></style>
---
+++ gym-management-front/src/pages/user/c-pages/plan/private-custom.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>plan</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped></style>
